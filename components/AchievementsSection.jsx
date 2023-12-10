aec07876fe88f4d2c560acd4ed8521a5aa398e5f
yuanna03 <annabelyly@gmail.com>
2023-12-10 22:25:43 +0800

    update

--- components/AchievementsSection.jsx
+++ components/AchievementsSection.jsx
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-const achievementsList = [
-    {
-        id: "projects", // Add unique keys for each element
-        metric: "Projects",
-        value: "2",
-    },
-    {
-        id: "awards",
-        metric: "Awards",
-        value: "1",
-    },
-    {
-        id: "years",
-        metric: "Years",
-        value: "1",
-    }
-];
-
-const AchievementsSection = () => {
-    return (
-        <div className="xl:gap-3 sm:py-3 xl:px-12">
-            <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
-                {achievementsList.map((achievement) => (
-                    <div
-                        key={achievement.id} // Provide a unique key prop
-                        className="flex flex-col items-center justify-center mx-4"
-                    >
-                        <h2 className="text-white text-4xl font-bold">
-                            {achievement.value}
-                        </h2>
-                        <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-};
-
-export default AchievementsSection;
-=======
 import React from "react";
 
 const achievementsList = [
@@ -79,4 +36,3 @@ const AchievementsSection = () => {
     )
 }
 export default AchievementsSection;
->>>>>>> bd6b8b5a2e9f1ed020626a07e215639db395c947

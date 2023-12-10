ae40594c3b2b43fecac2f1a415635d0b19351d03
yuanna03 <annabelyly@gmail.com>
2023-12-10 22:30:02 +0800

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 
 const Footer = () => {
     return(
         <footer className="w-full border-t border-[#33353F]">
             <div className="mb-14 w-full container mx-auto p-4 flex justify-between items-center text-slate-400">
-                <span className="">Annabel Yu Li Yin</span>
+                <span className="">Yee Chian</span>
                 <p>Copyright &copy; All rights reserved</p>
             </div>
         </footer>
     );
 };
-=======
-import React from "react";
-
-const Footer = () => {
-    return(
-        <footer className="w-full border-t border-[#33353F]">
-            <div className="mb-14 w-full container mx-auto p-4 flex justify-between items-center text-slate-400">
-                <span className="">Annabel Yu Li Yin</span>
-                <p>Copyright &copy; All rights reserved</p>
-            </div>
-        </footer>
-    );
-};
->>>>>>> bd6b8b5a2e9f1ed020626a07e215639db395c947
 export default Footer;

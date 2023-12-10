bd6b8b5a2e9f1ed020626a07e215639db395c947
yuanna03 <148878758+yuanna03@users.noreply.github.com>
2023-12-10 21:51:30 +0800

    Add files via upload

---
+++ components/EmailSection.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import GithubIcon from "../public/github-icon.svg";
+import LinkedinIcon from "../public/linkedin-icon.svg";
+import InstagramIcon from "../public/instagram-icon.svg";
+import Link from "next/link";
+import Image from "next/image";
+import GmailIcon from "../public/gmail-icon.svg";
+
+const EmailSection = () => { 
+
+
+    return (
+        <main className="grid md:grid-cols-2 my-12 md:my-12 py-24 md:py-24 gap-4 relative" id="contact">
+            <div className="z-10">
+                <h5 className="text-4xl font-bold text-white my-2 ">Let's Connect</h5>
+                <p className="text-[#ADB7BE] mb-4 max-w-md mt-8 text-justify">
+                I'm currently looking for internship opportunities. For any inquires, you may drop a message. I'll get back to you ASAP!
+                </p>
+                <h4 className="pb-3 mt-6">Let's get in touch!</h4>
+                <div className=" flex flex-row gap-4 mt-6">
+                    <Link href="https://github.com/yuanna03">
+                        <Image src={GithubIcon} alt="Github Icon" />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/annabel-yu-li-yin-00009a254/">
+                        <Image src={LinkedinIcon} alt="Linkedin Icon" />
+                    </Link>
+                    <Link href="https://www.instagram.com/yuannabel/" className="mt-1">
+                        <Image src={InstagramIcon}/>
+                    </Link>
+                    <Link href="mailto:annabelyly@gmail.com" className="mt-1">
+                        <Image src={GmailIcon}/>
+                    </Link>
+
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default EmailSection;
---
+++ components/HeroSection.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import TypeWritter from "typewriter-effect";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+
+
+const HeroSection = () => {
+  return (
+    <main className="py-16 mt-16 md-flex md:shrink-0 ">
+      <div className="grid grid-cols-1 md:grid-cols-12 ">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="col-span-1 md:col-span-8 place-self-center justify-self-start"
+          >
+            <h1 className="font-extrabold">
+              {/* span text seperator -- for design*/}
+              <span className="text-white text-8xl ">
+                Hello, I'm{" "} 
+                <span className="text-transparent bg-clip-text sm:text-8xl md:text-9xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                Annabel Yu
+                </span>
+              </span>
+              <br></br>
+              <span className="text-7xl">I'm 
+              <TypeWritter
+                options={{
+                    autoStart: true,
+                    loop: true,
+                }}
+                onInit={(typewriter) => {
+                    typewriter
+                    .typeString`<span style="color:#FF77FF">Degree 2 Student</span>`
+                    .pauseFor(1500)
+                    .deleteAll()
+                    .typeString`<span style="color:#9E7BFF">Event Enthusiast</span>`
+                    .pauseFor(1500)
+                    .deleteAll()
+                    .typeString`<span style="color:#893BFF">Web Page Developer</span>`
+                    .pauseFor(1500)
+                    .deleteAll()
+                    .start();
+
+                }}
+              ></TypeWritter>
+              </span>
+            </h1>
+            <p className="text-gray-300 pt-8 text-base sm:text-lg mb-6 lg:text-xl">
+            I am a person who cherishes life, is keen to participate in various activities, and is always looking for new experiences. 
+            I am passionate, proactive and willing to explore new things.
+              Slide down to know more about me! 
+            </p>
+              <Link
+                href="#projects" 
+                className="px-12 mb-4 inline-block py-3 rounded-full mr-4 border-solid border-2 border-indigo-600 hover:bg-violet-600">
+                <span className="text-xl text-white">Projects</span>
+              </Link>
+             
+              <Link
+                href="https://drive.google.com/file/d/12XO1ZmHxHwYVQHQhoIoaruumlWVH2XLB/view?usp=drive_link"
+                className="px-6 inline-block py-3 rounded-full mr-4 border-solid border-2  border-pink-500 hover:bg-pink-400">
+                <span className="text-xl text-white">View my Resume</span>
+              </Link>
+          </motion.div>
+        <div className="col-span-4 md:col-span-4 place-self-center mt-12 lg:mt-0 pt-0 lg:pt-24">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+          <div className="w-[400px] h-[400px] relative z-0">
+            <img
+              src="/Hero-Section.png"
+              alt="hero section"
+              className=" transform rounded-full flex relative -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={450}
+              height={450}
+            />
+          </div>
+          </motion.div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default dynamic(() => Promise.resolve(HeroSection), {ssr: false});
